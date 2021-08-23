cbb05b42cea92c9e77c8edc41f5eb33bced12109
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2021-08-23 16:38:46 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var express=require('express')
+var app=express()
+app.use(express.urlencoded({extended:true}))
+app.listen(8089,()=>{
+    console.log("started")
+})
+app.get("/",(req,res)=>{
+res.send("Hello from server")
+})
+app.post("/",(req,res)=>{
+    console.log(req.body)
+})
