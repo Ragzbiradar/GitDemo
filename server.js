f37a8b48dbb7f72028b34fa279fd8531f716ae58
Raghavendra <53818125+Ragzbiradar@users.noreply.github.com>
2021-08-24 20:53:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.listen(5055,()=>{
 app.get("/",(req,res)=>{
 res.send("Hello from server")
 })
+app.get("/home",(req,res)=>{
+res.send("Hello from server home")
+})
 app.post("/",(req,res)=>{
     console.log(req.body)
 })
